abb577d3d25faa7495764f102d0bb882424921cf
bterrific2008 <bterrific2008@gmail.com>
2015-06-15 17:55:53 -0700

    Simplified it a bit

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -14,31 +14,22 @@ def whoWinsRPS(p1, p2):
     p1 = p1.lower()
     p2 = p2.lower()
 
+    if p1 == p2:
+        return "Players Draw!"
+
     if p1 == r:
         if p2 == s:
             return "Player 1 Wins!"
-        if p2 == p:
-            return "Player 2 Wins!"
-        if p2 == r:
-            return "Players Draw!"
 
     if p1 == p:
         if p2 == r:
             return "Player 1 Wins!"
-        if p2 == s:
-            return "Player 2 Wins!"
-        if p2 == p:
-            return "Players Draw!"
-
+      
     if p1 == s:
         if p2 == p:
             return "Player 1 Wins!"
-        if p2 == r:
-            return "Player 2 Wins!"
-        if p2 == s:
-            return "Players Draw!"
-    
-    return "Error 404"
+        
+    return "Player 2 Wins!"
 
 def main():
     print "*******************************"
